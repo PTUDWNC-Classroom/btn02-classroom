15d15006d3a518cd3eff043d6fd0f0531909f37a
Đức Huỳnh Thanh <cart.huynh.goldenowl@gmail.com>
2021-12-19 00:16:40 +0700

    fix: add student Id

--- src/Components/User/StudentIDListItem.jsx
+++ src/Components/User/StudentIDListItem.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import {
   Grid,
   Typography,
@@ -14,8 +14,18 @@ import { useTheme } from "@mui/material/styles"
 import useMediaQuery from "@mui/material/useMediaQuery"
 
 import CancelButton from "../Button/CancelButton"
+import { useForm } from "react-hook-form"
+import classroomAxios from "../DataConnection/axiosConfig"
+import { yupResolver } from "@hookform/resolvers/yup"
 
-export function EditIDActionListItem({ handleEdit, handleAddStudentId }) {
+import * as yup from "yup"
+import { ClassroomContext } from "../../context/ClassroomContext"
+
+const schema = yup.object().shape({
+  studentId: yup.string().required("Please enter your student ID!"),
+})
+
+export function EditIDActionListItem({ handleEdit }) {
   return (
     <ListItem>
       <Grid container alignItems="center">
@@ -25,16 +35,13 @@ export function EditIDActionListItem({ handleEdit, handleAddStudentId }) {
             variant="outlined"
             onClick={handleEdit}
             style={{ marginRight: "1rem" }}
+            type="reset"
           >
             Cancel
           </CancelButton>
         </Grid>
         <Grid item>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={handleAddStudentId}
-          >
+          <Button color="primary" variant="contained" type="submit">
             Save
           </Button>
         </Grid>
@@ -43,87 +50,134 @@ export function EditIDActionListItem({ handleEdit, handleAddStudentId }) {
   )
 }
 
-export default function StudentIDListItem({
-  edit,
-  studentId,
-  handleEdit,
-  handleChangeStudentId,
-  handleAddStudentId,
-}) {
+export default function StudentIDListItem({ studentId, handleAddStudentId }) {
   const theme = useTheme()
+  const { login, user } = useContext(ClassroomContext)
+  const [edit, setEdit] = React.useState(false)
   const matchDownSM = useMediaQuery(theme.breakpoints.down("sm"))
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const handleEdit = () => {
+    clearErrors("studentId")
+    setEdit(!edit)
+  }
+
+  const onSubmit = async (data) => {
+    try {
+      console.log(data)
+      const response = await classroomAxios.post(`user/add-student-id`, {
+        //userId: user._id,
+        studentId: data.studentId,
+      })
+
+      if (response) {
+        localStorage.setItem("isSocialLogin", JSON.stringify(response.data))
+      }
+      login(response.data)
+      setEdit(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <>
-      <ListItem>
-        <Grid container alignItems="center">
-          {matchDownSM ? (
-            <>
-              <Grid item xs>
-                <ListItemText>
-                  <b>Student ID</b>
-                </ListItemText>
-              </Grid>
-              {!studentId && (
-                <Grid item xs={1}>
-                  <IconButton onClick={handleEdit}>
-                    <EditIcon />
-                  </IconButton>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ListItem>
+          <Grid container alignItems="center">
+            {matchDownSM ? (
+              <>
+                <Grid item xs>
+                  <ListItemText>
+                    <b>Student ID</b>
+                  </ListItemText>
                 </Grid>
-              )}
-              <Grid item xs={12}>
-                {edit ? (
-                  <TextField
-                    size="small"
-                    fullWidth
-                    value={studentId}
-                    onChange={handleChangeStudentId}
-                    helperText="You have 1 time to enter your student ID"
-                    autoFocus
-                  />
-                ) : (
-                  <Typography variant="subtitle1">{studentId}</Typography>
+                {!studentId && (
+                  <Grid item xs={1}>
+                    <IconButton onClick={handleEdit}>
+                      <EditIcon />
+                    </IconButton>
+                  </Grid>
                 )}
-              </Grid>
-            </>
-          ) : (
-            <>
-              <Grid item sm={3} xs={12}>
-                <ListItemText>
-                  <b>Student ID</b>
-                </ListItemText>
-              </Grid>
+                <Grid item xs={12}>
+                  {edit ? (
+                    <>
+                      <TextField
+                        {...register("studentId")}
+                        size="small"
+                        fullWidth
+                        helperText="You have 1 time to enter your student ID"
+                        //autoFocus
+                      />
+                      {errors.studentId && (
+                        <Typography color="error">
+                          {errors.studentId?.message}
+                        </Typography>
+                      )}
+                    </>
+                  ) : (
+                    <Typography variant="subtitle1">
+                      {user.studentId}
+                    </Typography>
+                  )}
+                </Grid>
+              </>
+            ) : (
+              <>
+                <Grid item sm={3} xs={12}>
+                  <ListItemText>
+                    <b>Student ID</b>
+                  </ListItemText>
+                </Grid>
 
-              <Grid item sm={8} xs={11}>
-                {edit ? (
-                  <TextField
-                    size="small"
-                    value={studentId}
-                    onChange={handleChangeStudentId}
-                    helperText="You have 1 time to enter your student ID"
-                    autoFocus
-                  />
-                ) : (
-                  <Typography variant="subtitle1">{studentId}</Typography>
-                )}
-              </Grid>
-              {!studentId && (
-                <Grid item xs={1}>
-                  <IconButton onClick={handleEdit}>
-                    <EditIcon />
-                  </IconButton>
+                <Grid item sm={8} xs={11}>
+                  {edit ? (
+                    <TextField
+                      {...register("studentId")}
+                      size="small"
+                      helperText="You have 1 time to enter your student ID"
+                      autoFocus
+                    />
+                  ) : (
+                    <Typography variant="subtitle1">
+                      {user.studentId}
+                    </Typography>
+                  )}
                 </Grid>
-              )}
-            </>
-          )}
-        </Grid>
-      </ListItem>
-      {edit && (
-        <EditIDActionListItem
-          handleEdit={handleEdit}
-          handleAddStudentId={handleAddStudentId}
-        />
-      )}
+
+                {!user.studentId && (
+                  <Grid item xs={1}>
+                    <IconButton onClick={handleEdit}>
+                      <EditIcon />
+                    </IconButton>
+                  </Grid>
+                )}
+                <Grid item sm={3} xs={12}></Grid>
+                <Grid item sm={8} xs={11}>
+                  {errors.studentId && (
+                    <Typography color="error">
+                      {errors.studentId?.message}
+                    </Typography>
+                  )}
+                </Grid>
+              </>
+            )}
+          </Grid>
+        </ListItem>
+        {edit && (
+          <EditIDActionListItem
+            handleEdit={() => handleEdit()}
+            handleAddStudentId={handleAddStudentId}
+          />
+        )}
+      </form>
     </>
   )
 }
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -9,8 +9,6 @@ import {
   ListItemText,
 } from "@mui/material"
 import { styled } from "@mui/system"
-import classroomAxios from "../../Components/DataConnection/axiosConfig"
-
 import StudentIDListItem from "../../Components/User/StudentIDListItem"
 import { ClassroomContext } from "../../context/ClassroomContext"
 
@@ -19,35 +17,7 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
 }))
 
 export default function Profile() {
-  const [edit, setEdit] = React.useState(false)
-  const [studentId, setStudentId] = React.useState("")
   const { user } = useContext(ClassroomContext)
-
-  const handleEdit = () => {
-    setEdit(!edit)
-  }
-
-  const handleChangeStudentId = (e) => {
-    setStudentId(e.target.value)
-  }
-
-  const handleAddStudentId = async () => {
-    if (studentId) {
-      try {
-        const response = await classroomAxios.post(`user/add-student-id`, {
-          userId: user._id,
-          studentId: studentId,
-        })
-
-        if (response) {
-          localStorage.setItem("isSocialLogin", JSON.stringify(response.data))
-        }
-        setEdit(false)
-      } catch (error) {
-        console.error(error)
-      }
-    } else alert("Please enter your student ID before saving!")
-  }
 
   useEffect(() => {
     document.title = "Profile"
@@ -89,13 +59,14 @@ export default function Profile() {
                   </Grid>
                   <ListItemText />
                 </ListItem>
+                {/* <FormProvider {...methods}> */}
                 <StudentIDListItem
-                  studentId={user.studentId}
-                  handleChangeStudentId={handleChangeStudentId}
-                  handleAddStudentId={handleAddStudentId}
-                  edit={edit}
-                  handleEdit={handleEdit}
+                //handleChangeStudentId={handleChangeStudentId}
+                //handleAddStudentId={handleAddStudentId}
+                //edit={edit}
+                //handleEdit={handleEdit}
                 />
+                {/* </FormProvider> */}
               </List>
             </Grid>
           </Grid>
